e94cf32ca9bcacbfea74768a53bee0eb58fa9d47
Hamza ATILGAN <122400602+hamzaatilgan@users.noreply.github.com>
2023-09-05 17:24:02 +0300

    Update js api-url

---
+++ 5/assets/main.js
@@ -0,0 +1,7 @@
+const apiURL =
+  "https://raw.githubusercontent.com/cuneydbolukoglu/challenge/main/challenge-carousel.json";
+const carosel = document.querySelector(".Carosel");
+const leftButton = document.querySelector(".left");
+const rightButton = document.querySelector(".right");
+const incircle = document.querySelector(".incircle");
+let currentslide = 0;
